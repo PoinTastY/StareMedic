5d4489e48d9f931d596e10cabddc51fb39e1ef6b
Erick Garcia <erick-noel_123@outlook.com>
2024-02-11 16:44:56 -0600

    Creation of a Flyout for the mainpage

--- StareMedic/MainPage.xaml.cs
+++ StareMedic/MainPage.xaml.cs
@@ -1,15 +1,26 @@
 ﻿namespace StareMedic;
 
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using StareMedic.Models;
 using StareMedic.Views;
 using StareMedic.Views.Viewers;
+using Microsoft.Maui.Controls;
+
+
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+
+       
+    }
+
+    private void OnHamburguerClicked(object sender, EventArgs e)
+    {
+        sidePanel.IsVisible = !sidePanel.IsVisible;
+    }
 
 	//remember to register pages on appshell first, or you are not going nowhere
     private void BtnPacientes_Clicked(object sender, EventArgs e)
