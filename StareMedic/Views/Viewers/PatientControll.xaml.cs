57f299ccff3f46fb3cd739d1f01853c45221ec53
Kevin Becerra <kbecerra774@gmail.com>
2024-06-25 22:01:59 -0600

    test for issue #120

--- StareMedic/Views/Viewers/PatientControll.xaml.cs
+++ StareMedic/Views/Viewers/PatientControll.xaml.cs
@@ -9,6 +9,8 @@ public partial class PatientControll : ContentPage
     private Cercano cercano;
     private Fiador fiador;
 
+    public event EventHandler<PatientSelectedEventArgs> PatientSelected;
+
     public PatientControll(Patient pacientEdit)
 	{
 		InitializeComponent();
@@ -186,6 +188,15 @@ public partial class PatientControll : ContentPage
                 else
                     await DisplayAlert("Error", "Hubo un problema registrando al paciente, intentalo mas tarde.", "Ok");
                 //go back
+                try
+                {
+                    PatientSelected?.Invoke(this, new PatientSelectedEventArgs { SelectedPatient = paciente });
+                    //await Navigation.PopModalAsync(); // esta en el evento mejor
+                }
+                catch
+                {
+                    
+                }
                 await Shell.Current.GoToAsync("..");
             }
         }
