c669d2444fd38d09c26ca83fcacedc3d7a0e3783
Kevin Becerra <kbecerra774@gmail.com>
2024-06-26 22:32:13 -0600

    progess on absolut

--- StareMedic/Models/GenerateAdmisionDoc.cs
+++ StareMedic/Models/GenerateAdmisionDoc.cs
@@ -43,7 +43,23 @@ namespace StareMedic.Models
                 var no_expediente = new Paragraph($"{caso.Id}").SetMaxWidth(200).SetFixedPosition(345, 785, 200);
                 canvas.Add(no_expediente);
 
+                var fecha_ingreso = new Paragraph($"{caso.FechaIngreso:dd/MM/yyyy}").SetMaxWidth(150).SetFixedPosition(90, 770, 150);
+                canvas.Add(fecha_ingreso);
 
+                var sexo = new Paragraph($"{patient.Sexo}").SetMaxWidth(50).SetFixedPosition(335, 770, 50);
+                canvas.Add(sexo);
+
+                var edad = new Paragraph($"{patient.Edad}").SetMaxWidth(150).SetFixedPosition(420, 770, 150);
+                canvas.Add(edad);
+
+                var nombre = new Paragraph($"{patient.Nombre}").SetMaxWidth(150).SetFixedPosition(90, 755, 150);
+                canvas.Add(nombre);
+
+                var estado_civil = new Paragraph($"{patient.EstadoCivil}").SetMaxWidth(50).SetFixedPosition(340, 755, 50);
+                canvas.Add(estado_civil);
+
+                var nacionalidad = new Paragraph($"{patient.Nacionalidad}").SetMaxWidth(150).SetFixedPosition(420, 755, 150);
+                canvas.Add(nacionalidad);
 
                 document.Close();
                 return true;
